358fe1956a803784cc2edc1f7603578bcc851ff3
TheUnnamedOneDesign <aditya07.11.04.12@gmail.com>
2025-09-14 21:03:17 +0530

    added calculator program and a basic template

---
+++ calculator.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int add(int a,int b)
+{
+    return a+b;
+}
+
+int sub(int a,int b)
+{
+    return a-b;
+}
+
+int mul(int a,int b)
+{
+    return a*b;
+}
+
+int main()
+{
+    cout<<"welcome to calculator";
+}
